b5c12dadffd983f15d83d2748b0ea2941551d11f
V-Ivanich <chipai_dom@mail.ru>
2021-06-17 21:55:15 +0300

    "scroll_new"

--- js/script.js
+++ js/script.js
@@ -24,3 +24,50 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = " block";
     dots[slideIndex - 1].className += " active";
 };
+
+$(document).ready(function(){
+  // Добавить плавную прокрутку до всех ссылок
+  $("a").on('click', function(event) {
+
+    // Убедись в этом что .hash имеет значение перед переопределением поведения по умолчанию
+    if (this.hash !== "") {
+      // Запретить поведение щелчка якоря по умолчанию
+      event.preventDefault();
+
+      // Хранить хэш
+      var hash = this.hash;
+
+      // Использование метода animate() jQuery для добавления плавной прокрутки страницы
+      // Необязательное число (800) указывает количество миллисекунд, необходимых для прокрутки до указанной области
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Добавить хэш (#) для URL-адреса после завершения прокрутки (поведение щелчка по умолчанию)
+        window.location.hash = hash;
+      });
+    } // Конец, если
+  });
+});
+
+
+//мой скролл
+
+/*
+var hiddenElement = document.getElementById("box");
+var btn = document.querySelector('.btn_1');
+
+var foot = document.getElementById("foo");
+var btn_2 = document.querySelector('.btn_2');
+
+function handleButtonClick() {
+   hiddenElement.scrollIntoView({block: "start", behavior: "smooth"});
+}
+
+function footClick() {
+   foot.scrollIntoView({block: "end", behavior: "smooth"});
+}
+
+btn.addEventListener('click', handleButtonClick);
+btn_2.addEventListener('click', footClick);
+*/
